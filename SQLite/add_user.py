62eab655e809e026e97134470afd018f78eeb879
khaoulaMili123 <khawla_mili@yahoo.com>
2025-09-21 18:34:59 +0200

    sqlite et front

--- SQLite/add_user.py
+++ SQLite/add_user.py
@@ -2,31 +2,49 @@ import sqlite3
 import bcrypt
 import os
 
-
 sqlite_path = "./data/bdd_connexion.sqlite"
 
-os.makedirs("./data", exist_ok=True) #vérifier que data existe
+os.makedirs("./data", exist_ok=True)  # vérifier que data existe
 
 def add_user(nom_utilisateur, identifiant, mot_de_passe, role_id):
-    """Ajoute un utilisateur avec mot de passe haché"""
-
-    hashed_password = bcrypt.hashpw(mot_de_passe.encode('utf-8'), bcrypt.gensalt())
+    """Ajoute un utilisateur après vérification"""
 
     conn = sqlite3.connect(sqlite_path)
     cur = conn.cursor()
+
+      # Vérifier si l'identifiant existe déjà pour ce rôle
+    cur.execute(
+        "SELECT mot_de_passe FROM utilisateurs WHERE identifiant = ? AND role_id = ?",
+        (identifiant, role_id)
+    )
+    result = cur.fetchone()
+
+    if result:
+        # Utilisateur existe déjà pour ce rôle → vérifier le mot de passe
+        stored_hashed_password = result[0]
+
+        if bcrypt.checkpw(mot_de_passe.encode('utf-8'), stored_hashed_password):
+            print("L'utilisateur existe déjà")
+        else:
+            print("Mot de passe incorrect.")
+        conn.close()
+        return
+
+    # Sinon (identifiant pas encore utilisé pour ce rôle) → on ajoute
+    hashed_password = bcrypt.hashpw(mot_de_passe.encode('utf-8'), bcrypt.gensalt())
     cur.execute(
         "INSERT INTO utilisateurs (nom_utilisateur, identifiant, mot_de_passe, role_id) VALUES (?, ?, ?, ?)",
         (nom_utilisateur, identifiant, hashed_password, role_id)
-        )
+    )
     conn.commit()
     conn.close()
-    print(f"Utilisateur {nom_utilisateur} ajouté avec succès.")
+    print(f"L'tilisateur {nom_utilisateur} ajouté avec succès (identifiant '{identifiant}', rôle {role_id}).")
 
 if __name__ == "__main__":
-    # Demande à l’utilisateur les infos
+
     nom = input("Nom complet : ")
     identifiant = input("Identifiant : ")
     mdp = input("Mot de passe : ")
     role = int(input("Role ID (1=enseignant, 2=etudiant, 3=administrateur) : "))
 
-    add_user(nom, identifiant, mdp, role)    
+    add_user(nom, identifiant, mdp, role)
